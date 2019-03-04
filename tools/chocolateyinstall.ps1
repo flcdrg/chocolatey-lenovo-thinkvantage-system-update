9984a627b955519077d60fb2ef06ad12cd49cd3d
David Gardiner <david@gardiner.net.au>
2019-03-05 00:27:16 +1030

    Add update script

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -3,6 +3,7 @@
 $packageName= 'lenovo-thinkvantage-system-update'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/systemupdate5.07.0074.exe'
+$checksum   = ''
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +11,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'Lenovo System Update'
-  checksum      = '9EE92F2EBCFB59E0FBC3B9D06740E77F93FD9CFB06145E17E7B519FF9261252B'
+  checksum      = $checksum
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0)
---
+++ update.ps1
@@ -0,0 +1,23 @@
+import-module au
+
+function global:au_SearchReplace {
+    @{
+        'tools\chocolateyInstall.ps1' = @{
+            "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+            "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+        }
+     }
+}
+
+function global:au_GetLatest {
+
+    # Unfortunately, they're including a Byte Order Mark, so we have to trim that off
+    $response = Invoke-RestMethod -Uri "https://download.lenovo.com/ibmdl/pub/pc/pccbbs/agent/SSClientCommon/HelloLevel_9_34_00.xml"
+    $xml = [xml] $response.Substring(3)
+    $version = $xml.LevelDescriptor.Version
+
+    $Latest = @{ URL32 = $url32; Version = $version }
+    return $Latest
+}
+
+update
