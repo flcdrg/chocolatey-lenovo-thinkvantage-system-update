442f937fc881ee115e54fd990e8de96c40dfe48b
David Gardiner <david@gardiner.net.au>
2019-03-05 00:47:32 +1030

    Version 5.07.0078
    - using update.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 
 $packageName= 'lenovo-thinkvantage-system-update'
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/systemupdate5.07.0074.exe'
-$checksum   = ''
+$url        = 'https://download.lenovo.com/pccbbs/thinkvantage_en/systemupdate5.07.0078.exe'
+$checksum   = 'b18bd7e9617c1225f8d8e39e4b4983ed89f1629c9284e48d74d2e8917728bd71'
 
 $packageArgs = @{
   packageName   = $packageName
--- update.ps1
+++ update.ps1
@@ -6,9 +6,6 @@ function global:au_SearchReplace {
             "(^[$]url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
             "(^[$]checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
         }
-        "$($Latest.PackageName).nuspec" = @{
-            "(\<releaseNotes\>).*?(\</releaseNotes\>)" = "`${1}$($Latest.ReleaseNotes)`$2"
-        }
      }
 }
 
@@ -18,13 +15,25 @@ function global:au_GetLatest {
     $response = Invoke-RestMethod -Uri "https://download.lenovo.com/ibmdl/pub/pc/pccbbs/agent/SSClientCommon/HelloLevel_9_34_00.xml"
     $xml = [xml] $response.Substring(3)
     $version = $xml.LevelDescriptor.Version
-
+    $buildDate = $xml.LevelDescriptor.BuildDate
+    
     $Latest = @{
         URL32 = "https://download.lenovo.com/pccbbs/thinkvantage_en/systemupdate$($version).exe";
-        ReleaseNotes = "https://download.lenovo.com/pccbbs/thinkvantage_en/systemupdate$($version).txt"
+        ReleaseNotes = "$buildDate release - https://download.lenovo.com/pccbbs/thinkvantage_en/systemupdate$($version).txt"
         Version = $version 
     }
     return $Latest
 }
 
-update
+function global:au_AfterUpdate
+{ 
+    $nuspecFileName = $Latest.PackageName + ".nuspec"
+    $nu = Get-Content $nuspecFileName -Raw -Encoding UTF8
+    $nu = $nu -replace "(?smi)(\<releaseNotes\>).*?(\</releaseNotes\>)", "`${1}$($Latest.ReleaseNotes)`$2"
+    
+    $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
+    $NuPath = (Resolve-Path $NuspecFileName)
+    [System.IO.File]::WriteAllText($NuPath, $nu, $Utf8NoBomEncoding)
+}
+
+update -NoReadme
